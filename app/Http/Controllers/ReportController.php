d4cf406ec9256edacd3114f498ae070303e43851
badadarr <badar.maulana.techno@gmail.com>
2025-10-06 14:56:13 +0700

    Fixing to handle inertia pagination

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -8,23 +8,25 @@ use Inertia\Inertia;
 
 class ReportController extends Controller
 {
-    public function __construct(private ReportService $reportService) {}
+    public function __construct(private ReportService $reportService)
+    {
+    }
 
     public function outstanding(Request $request)
     {
         $filters = $request->only(['start_date', 'end_date']);
-        
+
         return Inertia::render('Report/Outstanding', [
-            'orders' => $this->reportService->getOutstandingReport($filters)->toArray()
+            'orders' => $this->reportService->getOutstandingReport($filters)
         ]);
     }
 
     public function topCustomers(Request $request)
     {
         $filters = $request->only(['start_date', 'end_date', 'limit']);
-        
+
         return Inertia::render('Report/TopCustomers', [
-            'customers' => $this->reportService->getTopCustomers($filters)->toArray()
+            'customers' => $this->reportService->getTopCustomers($filters)
         ]);
     }
 }
